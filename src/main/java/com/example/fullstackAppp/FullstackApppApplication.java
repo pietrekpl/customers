d576bfebbfdb1e4c9c50975d2eeb74395ff736e9
Piotr Ludynia <pietrekpl18@gmail.com>
2023-07-05 10:30:14 +0200

    initial commit

---
+++ src/main/java/com/example/fullstackAppp/FullstackApppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackAppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fullstackAppp/FullstackApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackAppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
